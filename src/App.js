b33b0548ac273ff29cea9d55b68e9e756755c565
Vu Nguyen <tuanvu.vn007@gmail.com>
2017-06-24 20:42:13 +0700

    update font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Note from './components/Note'
+import Composer from './components/Composer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Composer onSubmit={console.log}/>
         <Note title='note title' content='note content' />
       </div>
     );
