224004370e18935bb846eed3fd2854dfb36b57e7
Vu Nguyen <tuanvu.vn007@gmail.com>
2017-06-25 14:01:14 +0700

    add masonry layout

--- src/components/NoteList/index.js
+++ src/components/NoteList/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './note-list.css';
 import Note from '../Note';
+import Mansory from 'react-masonry-component'
+
+const options = {
+  fitWidth: true
+}
+
+const noteStyle = {
+  width: 200
+}
 
 const NodeList = ({ notes }) => {
   return (
-    <div className="note-list-container">
-      {notes.map(note => <Note title={note.title} content={note.content} />)}
-    </div>
+    <Mansory className="note-list-container" options={options}>
+      {notes.map(note => <Note style={noteStyle} title={note.title} content={note.content} />)}
+    </Mansory>
   );
 };
 
